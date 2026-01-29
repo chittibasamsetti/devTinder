851dec8ac62054b3cda3c953a1766ed8232a7a95
chittibasamsetti <chittibasamsetti@gmail.com>
2026-01-30 00:25:05 +0530

    route handlers, middlewares

--- src/app.js
+++ src/app.js
@@ -1,16 +1,83 @@
+// const express=require("express");
+
+// const app=express();
+
+
+// app.get("/ab?c",(req,res)=>{
+//     res.send("This is get user server");
+// })
+
+// app.post("/user",(req,res)=>{
+//     res.send({"name":"chitti","age":23});
+// })
+// app.use("/test",(req,res)=>{
+//     res.send("This is test server just for testing");
+// });
+
+// app.use("/",(req,res)=>{
+//     res.send("welcome to the home page");
+// })
+
+// app.use("/dest",(req,res)=>{
+// res.send("This is destination server for testing");
+// })
+
+// app.listen(7777,()=>{
+//     console.log("Server is running on port 7777");
+
+// });
+
+
+
+
+
+
+
+
+// ROUTE HANDLERS AND MIDDLEWARS
+// const express=require("express");
+// const app=express();
+
+// app.get("/",
+     
+//     [(req,res,next)=>{
+        
+//         // res.send("this is route handler1")
+//         next();
+//     }, 
+    
+//     (req,res,next)=>{
+//         next()
+//         // res.send("this is route handler2")
+//         }],
+//     (req,res,next)=>{
+//         next()
+//         // res.send("this is route handler3")
+//     },
+//     (req,res,next)=>{
+        
+//         res.send("this is route handler4")
+//     }
+// );
+
+// app.listen(7777,()=>{
+//     console.log("server is running on the port 7777");
+// })
+
+
+
+
+
 const express=require("express");
-
 const app=express();
+const{userAuth}=require("./middlewares/auths");
 
-app.use("/test",(req,res)=>{
-    res.send("This is test server just for testing");
+app.use("/",userAuth,(req,res,next)=>{
+    res.send("welcome to home page");
 });
 
-app.use("/dest",(req,res)=>{
-res.send("This is destination server for testing");
-})
-
-app.listen(7777,()=>{
-    console.log("Server is running on port 7777");
-
+app.get("/user",(req,res)=>{
+    res.send("User data accessed");
 });
+
+app.listen(7777);
